ff3ee72e70ae9371b437840f9a4274bc5fffa69e
Jon Rice <revyolution1120@gmail.com>
2019-06-19 20:10:55 -0700

    Fish tree done.

--- BuildAZoo/Classes/Fish.cs
+++ BuildAZoo/Classes/Fish.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace BuildAZoo.Classes
 {
-    class Fish :Animal
+    public abstract class Fish :Animal
     {
+        public override int Legs { get; set; } = 0;
+
+        public override bool Warmblooded { get; set; } = false;
+
+        public override string Eat()
+        {
+            return "I eat plankton";
+        }
+
+        public override string Call()
+        {
+            return "Bubbles";
+        }
+
+        public override string Hunt()
+        {
+            return "I hunt as a school";
+        }
+
+        public override bool Migrate()
+        {
+            return false;
+        }
+
+        public override string Sleep()
+        {
+            return "I sleep in the water";
+        }
+
+
     }
 }
